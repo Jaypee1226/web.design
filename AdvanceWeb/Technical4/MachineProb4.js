37811ee9639338385c9b7d839194a9cdd9c15db7
John Patrick San Roque <sanroquejohnpatrick26@gmail.com>
2025-03-18 17:36:02 +0800

    Update

--- AdvanceWeb/Technical4/MachineProb4.js
+++ AdvanceWeb/Technical4/MachineProb4.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-
     document.getElementById("food-description").style.display = "none";
 
     let cards = document.querySelectorAll(".card");
@@ -10,19 +9,18 @@ document.addEventListener("DOMContentLoaded", function () {
             let img = card.getAttribute("data-image");
             let cuisine = card.getAttribute("data-cuisine");
             let cooking = card.getAttribute("data-cooking");
-            let ingredients = card.getAttribute("data-ingredients");
+            let dishes = card.getAttribute("data-dishes"); // Corrected
 
-            openFoodCard(name, desc, img, cuisine, cooking, ingredients);
+            openFoodCard(name, desc, img, cuisine, cooking, dishes);
         });
     });
-
 
     document.getElementById("main-menu").addEventListener("click", function () {
         closeFoodCard();
     });
 });
 
-function openFoodCard(name, desc, img, cuisine, cooking, ingredients) {
+function openFoodCard(name, desc, img, cuisine, cooking, dishes) {
     let foodCard = document.getElementById("food-description");
 
     document.getElementById("food-image").src = img;
@@ -30,9 +28,9 @@ function openFoodCard(name, desc, img, cuisine, cooking, ingredients) {
     document.getElementById("food-text").innerText = desc;
     document.getElementById("food-cuisine").innerText = cuisine;
     document.getElementById("food-cooking").innerText = cooking;
-    document.getElementById("food-ingredients").innerText = ingredients;
+    document.getElementById("food-ingredients").innerText = dishes;
 
-    foodCard.style.display = "block"; 
+    foodCard.style.display = "flex"; 
     setTimeout(() => {
         foodCard.classList.add("show");
     }, 10);
@@ -44,6 +42,6 @@ function closeFoodCard() {
     foodCard.classList.remove("show");
 
     setTimeout(() => {
-        foodCard.style.display = "none"; 
+        foodCard.style.display = "none";
     }, 300);
 }
